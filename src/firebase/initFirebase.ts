c6d559674b60a0884208d5678967f4faf7ff2303
Clint Duran <clint.duran@cireson.com>
2021-09-30 00:59:04 +0800

    fixing

--- src/firebase/initFirebase.ts
+++ src/firebase/initFirebase.ts
@@ -4,13 +4,13 @@ import { getPerformance } from 'firebase/performance';
 import { Firestore } from 'firebase/firestore';
 
 const clientCredentials = {
-    apiKey: "AIzaSyC7xdlBJDiVnya3QZbQl8j-WNINohD3Smo",
-    authDomain: "todos-d44b1.firebaseapp.com",
-    projectId: "todos-d44b1",
-    storageBucket: "todos-d44b1.appspot.com",
-    messagingSenderId: "223128346936",
-    appId: "1:223128346936:web:b922b64977edf2464d21cd",
-    measurementId: "G-PF61LGBPC2"
+    apiKey: <API_KEY>,
+    authDomain: <authDomain>,
+    projectId: <ProjectID>,
+    storageBucket: <StorageBucket>,
+    messagingSenderId: <MessagingSenderId>,
+    appId: <AppID>,
+    measurementId: <MeasurementId>
 }
 
 export default function initFirebase() {
